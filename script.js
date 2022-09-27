c23cef287b41665a438f650011536d5faf9fa6dd
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-09-27 14:31:56 -0500

    first push

---
+++ script.js
@@ -0,0 +1,52 @@
+// 1
+// let 
+
+
+// 2
+let num = 20;
+
+
+// 3
+num = 15
+
+
+// 4
+num += 7;
+
+
+// 5
+num--;
+
+
+// 6
+num = "Not a number";
+
+
+// 7
+let blank = undefined;
+
+
+// 8
+console.log(blank); 
+
+
+// 9
+blank = null;
+
+
+// bonus
+
+// b1
+const quarter = 0.25, dime = 0.10, nickle = 0.05, penny = 0.01;
+
+
+// b2
+console.log (quarter,dime,nickle,penny)
+
+
+// b3
+let letters = "qwertyuiop";
+
+
+// b4
+console.log(letters[9]);
